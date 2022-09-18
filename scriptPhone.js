0811d72cc6a76a8c0e6efb2c433a3d547d8d1daf
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2022-09-18 19:25:31 +0200

    change font and background

--- scriptPhone.js
+++ scriptPhone.js
@@ -1,15 +1,18 @@
 const widget = new ListWidget()
-widget.backgroundColor = Color.red()
+let gradient = new LinearGrandient()
+gradient.location = [0, 1]
+gradient.colors = [new Color('141414'), new Color('13233F')]
+// widget.backgroundColor = Color.red()
+widget.backgroundColorGradient = gradient
 
 const req = new Request('http://51.77.149.101/apiChrome.php?cp=77410')
 const response = await req.loadJSON()
 
 for (const i of response) {
-    widget.addText(i.title).textColor = Color.white()
-    widget.addText(i.rue).textColor = Color.white()
-    widget.addText(i.cp).textColor = Color.white()
-    widget.addText(i.prix).textColor = Color.white()
-    widget.addText(i.date).textColor = Color.white()
+    setTextWidget(widget.addText(i.title))
+    setTextWidget(widget.addText(i.rue))
+    setTextWidget(widget.addText(i.cp))
+    setTextWidget(widget.addText(`${i.prix} € le ${i.date}`))
 }
 
 const now = Date.now()
@@ -18,5 +21,9 @@ widget.refreshAfterDate = new Date(now + (180 * 60 * 1000))
 Script.setWidget(widget)
 Script.complete()
 
-widget.presentMedium()
-// widget.presentExtraLarge()
+widget.presentLarge()
+
+function setTextWidget(element) {
+    element.font = Font.systemFont(12)
+    element.textColor = Color.white()
+}
