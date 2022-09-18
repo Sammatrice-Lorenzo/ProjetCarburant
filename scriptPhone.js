13d1f8ed55cb0798c8f16224f19dd1d553fa9637
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2022-09-18 20:28:32 +0200

    fix color and display

--- scriptPhone.js
+++ scriptPhone.js
@@ -1,23 +1,18 @@
 const widget = new ListWidget()
-const g = new LinearGradient()
-g.colors = [new Color('141414'), new Color('13233F')]
-g.location = [0, 1]
-// widget.backgroundColor = Color.red()
-widget.backgroundColorGradient = g
+
+// const g = new LinearGradient()
+// g.colors = [new Color('141414'), new Color('13233F')]
+// g.location = [0, 1]
+// widget.backgroundColorGradient = g
+widget.backgroundColor = new Color('13233F')
 
 const req = new Request('http://51.77.149.101/apiChrome.php?cp=77410')
 const response = await req.loadJSON()
 
 for (const i of response) {
-    let title = widget.addText(i.title)
-    let priceAndDate = widget.addText(`${i.prix} € le ${i.date}`)
-    let avenue = widget.addText(i.rue)
-    let cp = widget.addText(i.cp)
-
-    setTextWidget(title, 8)
-    setTextWidget(priceAndDate, 8)
-    setTextWidget(avenue, 6)
-    setTextWidget(cp, 6)
+    let text = widget.addText(`${i.title} ${i.prix} € le ${i.date} à ${i.rue} ${i.cp}`)
+    text.font = Font.systemFont(8)
+    text.textColor = Color.white()
 }
 
 const now = Date.now()
@@ -27,8 +22,3 @@ Script.setWidget(widget)
 Script.complete()
 
 widget.presentLarge()
-
-function setTextWidget(element, sizeFont) {
-    element.font = Font.systemFont(sizeFont)
-    element.textColor = Color.white()
-}
