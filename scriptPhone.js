33aab50baefd3abe851e0e421485262efaaf20ed
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2022-09-18 18:05:58 +0200

    fix call response

--- scriptPhone.js
+++ scriptPhone.js
@@ -5,17 +5,11 @@ const req = new Request('http://51.77.149.101/apiChrome.php?cp=77410')
 const response = await req.loadJSON()
 
 for (const i of response) {
-    let title = response[i].title
-    let rue = response[i].rue
-    let cp = response[i].cp
-    let prix = response[i].prix
-    let date = response[i].date
-
-    widget.addText(title).textColor = Color.white()
-    widget.addText(rue).textColor = Color.white()
-    widget.addText(cp).textColor = Color.white()
-    widget.addText(prix).textColor = Color.white()
-    widget.addText(date).textColor = Color.white()
+    widget.addText(i.title).textColor = Color.white()
+    widget.addText(i.rue).textColor = Color.white()
+    widget.addText(i.cp).textColor = Color.white()
+    widget.addText(i.prix).textColor = Color.white()
+    widget.addText(i.date).textColor = Color.white()
 }
 
 const now = Date.now()
