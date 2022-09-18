a79dbf81ada5909881d32d6e35253799825b7de2
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2022-09-18 17:00:11 +0200

    loop to read a repsonse

--- scriptPhone.js
+++ scriptPhone.js
@@ -1,15 +1,18 @@
 const widget = new ListWidget()
 
 const req = new Request('http://51.77.149.101/apiChrome.php?cp=77410')
-const response = await req.loadJSON()
+const response = req.loadJSON()
 
-const title = response['title']
-const rue = response['rue']
-const cp = response['cp']
-const prix = response['prix']
-const date = response['date']
+for (const i of response) {
+    let title = response[i]['title']
+    let rue = response[i]['rue']
+    let cp = response[i]['cp']
+    let prix = response[i]['prix']
+    let date = response[i]['date']
 
-console.log(req)
+    console.log(prix)
+}
+
 const now = Date.now()
 widget.refreshAfterDate = new Date(now + (180 * 60 * 1000))
 console.log(widget.refreshAfterDate)
