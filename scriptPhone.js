69650c810407775a7cff5f31aa66018cec374407
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2022-09-18 17:26:19 +0200

    fix repsonse and setup text

--- scriptPhone.js
+++ scriptPhone.js
@@ -1,22 +1,28 @@
 const widget = new ListWidget()
+widget.backgroundColor = Color.red()
 
 const req = new Request('http://51.77.149.101/apiChrome.php?cp=77410')
 const response = req.loadJSON()
 
 for (const i of response) {
-    let title = response[i]['title']
-    let rue = response[i]['rue']
-    let cp = response[i]['cp']
-    let prix = response[i]['prix']
-    let date = response[i]['date']
+    let title = response[i].title
+    let rue = response[i].rue
+    let cp = response[i].cp
+    let prix = response[i].prix
+    let date = response[i].date
 
-    console.log(prix)
+    widget.addText(title).textColor = Color.white()
+    widget.addText(rue).textColor = Color.white()
+    widget.addText(cp).textColor = Color.white()
+    widget.addText(prix).textColor = Color.white()
+    widget.addText(date).textColor = Color.white()
 }
 
 const now = Date.now()
 widget.refreshAfterDate = new Date(now + (180 * 60 * 1000))
-console.log(widget.refreshAfterDate)
+
 Script.setWidget(widget)
 Script.complete()
 
+widget.presentMedium()
 // widget.presentExtraLarge()
